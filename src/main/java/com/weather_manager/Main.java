5ef0888d4e0d4b817b8bac2a64e2bde4d369cba6
Patrick NAMEGNI <49642047+CodeShadowing95@users.noreply.github.com>
2025-04-11 12:31:38 +0200

    Exécution de l'app

--- src/main/java/com/weather_manager/Main.java
+++ src/main/java/com/weather_manager/Main.java
@@ -1,5 +1,8 @@
 package com.weather_manager;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,20 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Map<String, WeatherData> fakeMeteo = new HashMap<>();
+        fakeMeteo.put("Paris", new WeatherData("20", "22.5", "1015", "45"));
+        fakeMeteo.put("Lyon", new WeatherData("5", "16.2", "1009", "85"));
+        fakeMeteo.put("Marseille", new WeatherData("23", "25.0", "1012", "60"));
+
+        WeatherService mockService = city -> fakeMeteo.getOrDefault(city,
+                new WeatherData("Inconnu", "0.0", "0", "0")).toString();
+
+        MeteoManager manager = new MeteoManager(mockService);
+
+        System.out.println("Affichage des métés de Paris, Lyon, Marseille et Toulouse");
+        System.out.println(manager.afficherMeteo("Paris"));
+        System.out.println(manager.afficherMeteo("Lyon"));
+        System.out.println(manager.afficherMeteo("Marseille"));
+        System.out.println(manager.afficherMeteo("Toulouse")); // Non défini
     }
 }
